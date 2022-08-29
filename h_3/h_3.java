b1596c9b76ec5e1118cdbb79e37ae98ae557310c
Roman_Chub <rchub@inbox.ru>
2022-08-29 15:42:47 +0300

    first task is added

---
+++ h_3/h_3.java
@@ -0,0 +1,20 @@
+package h_3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class h_3 {
+
+    // Task 1
+    // Напишите программу, чтобы создать новый список массивов,
+    // добавить несколько цветов (строку) и вывести коллекцию на экран.
+
+    static List<String> ArrayLists(){
+    List<String> ListColors = new ArrayList<>();
+    ListColors.add("red");
+    ListColors.add("green");
+    ListColors.add("blue");
+    return ListColors;
+    }
+
+}
